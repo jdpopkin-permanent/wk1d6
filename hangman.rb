0ebff249fe4aabe9df4c579d53f4505fca199038
Jackson Popkin <jdpopkin@gmail.com>
2013-08-25 16:36:31 -0400

    Hangman now allows a human to act as either the guesser or the word selector.

--- hangman.rb
+++ hangman.rb
@@ -5,7 +5,20 @@ class HumanPlayer
   end
 
   def choose_word
+    length = 0
+    loop do 
+      puts "Enter the length of your word."
+      length = gets.chomp.to_i
+      next if length.nil? || length < 1
+      break
+    end
 
+    dummy_word = "" 
+    length.times do 
+      dummy_word << "_"
+    end
+
+    dummy_word
   end
 
   def guess_word
@@ -19,20 +32,40 @@ class HumanPlayer
     char
   end
 
+  def update_dummy(guess, dummy_word)
+    puts "Where does the letter #{guess} appear in your word? Enter comma-separated indices."
+    index_chars = gets.chomp.split(",")
+    indices = []
+
+    index_chars.each do |char|
+      indices << char.to_i if char.to_i > 0 && char.to_i <= dummy_word.length
+    end
+
+    indices.compact.each {|i| dummy_word[i - 1] = guess }
+    dummy_word
+  end
+
+  def word
+    "- well, how should I know?"
+  end
+
 end
 
 class ComputerPlayer
 
-  attr_reader :dictionary, :word
-  # attr_accessor :word
+  attr_reader :word, :difficulty
+  attr_accessor :dictionary_array, :guessed_letters, :available_letters
 
-  def initialize(dictionary, mode = :wordsmith)
-    @dictionary = dictionary
+  def initialize(dictionary, mode = :wordsmith, difficulty = :easy)
+    @dictionary_array = File.read(dictionary).split
     @mode = mode
+    @guessed_letters = []
+    @available_letters = ("a".."z").to_a
+    @difficulty = difficulty
   end
 
   def choose_word # set up word choice and return dummy word string
-    @word = File.readlines(dictionary).sample.chomp
+    @word = dictionary_array.sample
     
     dummy_word = ""
     word.length.times do
@@ -52,8 +85,30 @@ class ComputerPlayer
     dummy_word
   end
 
-  def guess_word
+  def update_dictionary(dummy_word)
+    dictionary_array.select! { |word| word.length == dummy_word.length }
+
+    unless difficulty == :easy
+      # remove words that don't match a regex of the dummy
+    end
+  end
+
+  def guess_word(dummy_word)
+    update_dictionary(dummy_word)
+
+    guess = ""
+    if difficulty == :easy
+      guess = available_letters.sample
+    elsif difficulty == :medium
+
+    elsif difficulty == :hard
+
+    end
+      
+    guessed_letters << guess
+    available_letters.delete(guess)
     
+    guess
   end
 
 end
@@ -94,9 +149,30 @@ class Hangman
       return [HumanPlayer.new, ComputerPlayer.new(dictionary)]
     when 1
       # computer is guessing, ask about difficulty
+      strength = -1
+      loop do
+        puts "Would you like a weak (0), medium (1), or strong (2) guesser?"
+        strength = gets.chomp.to_i
+        break if (0..2).include?(strength)
+      end
 
+      difficulty = get_difficulty(strength)
       # initialize human and computer players, then return them
+      # dic mode diffic
+      return [ComputerPlayer.new(dictionary, :guesser, difficulty), HumanPlayer.new]
     end
+  end
+
+  def get_difficulty(num)
+    case num
+    when 0
+      return :easy
+    when 1
+      return :medium
+    when 2
+      return :hard
+    end
+    raise "Error: difficulty not found"
   end
 
   def ask_number_humans
@@ -129,7 +205,7 @@ class Hangman
       old_dummy_word = dummy_word.dup
 
       # prompt guesser for guess
-      guess = guesser_player.guess_word
+      guess = guesser_player.guess_word(dummy_word)
       guesses << guess
       puts "Letter guessed: #{guess}"
       puts "Guessed letters: #{guesses.join(", ")}" # one function for this?
@@ -146,7 +222,7 @@ class Hangman
       end
     end
 
-    puts "The wordsmith wins! The correct word was #{wordsmith_player.word}." unless misses < miss_limit
+    puts "The wordsmith wins! The correct word was #{wordsmith_player.word}" unless misses < miss_limit
   end
 
 end
